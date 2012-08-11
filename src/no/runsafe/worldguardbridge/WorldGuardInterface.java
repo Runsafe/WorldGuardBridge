1c5435fbb36992419aba2d3551ff045cec7a9ced
Morten Nilsen <morten@runsafe.no>
2012-08-11 19:57:35 +0200

    Update bukkit reference Implement new methods needed by CreativeToolbox Check for WorldGuard on plugin enable

--- src/no/runsafe/worldguardbridge/WorldGuardInterface.java
+++ src/no/runsafe/worldguardbridge/WorldGuardInterface.java
@@ -6,6 +6,7 @@ import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.DefaultFlag;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import no.runsafe.framework.event.IPluginEnabled;
 import no.runsafe.framework.output.IOutput;
 import no.runsafe.framework.server.RunsafeLocation;
 import no.runsafe.framework.server.RunsafeWorld;
@@ -13,9 +14,10 @@ import no.runsafe.framework.server.player.RunsafePlayer;
 import org.bukkit.Server;
 import org.bukkit.plugin.Plugin;
 
+import java.awt.geom.Rectangle2D;
 import java.util.*;
 
-public class WorldGuardInterface
+public class WorldGuardInterface implements IPluginEnabled
 {
 	private Server server;
 	private WorldGuardPlugin worldGuard;
@@ -26,22 +28,19 @@ public class WorldGuardInterface
 		this.console = console;
 	}
 
-	private WorldGuardPlugin getWorldGuard(Server server)
+	@Override
+	public void OnPluginEnabled()
 	{
-		Plugin plugin = server.getPluginManager().getPlugin("WorldGuard");
-
-		if(plugin == null || !(plugin instanceof WorldGuardPlugin))
-			return null;
-
-		return (WorldGuardPlugin) plugin;
+		if (!serverHasWorldGuard())
+			console.write("Could not find WorldGuard on this server!");
 	}
 
 	public boolean serverHasWorldGuard()
 	{
-		if(this.worldGuard == null)
+		if (this.worldGuard == null)
 			this.worldGuard = this.getWorldGuard(this.server);
 
-		if(this.worldGuard != null)
+		if (this.worldGuard != null)
 			return true;
 
 		return false;
@@ -49,26 +48,26 @@ public class WorldGuardInterface
 
 	public boolean isInPvPZone(RunsafePlayer player)
 	{
-		if(player == null)
+		if (player == null)
 			console.fine("[isInPvPZone] player is null!");
 
-		if(!serverHasWorldGuard())
+		if (!serverHasWorldGuard())
 			return false;
 
-		if(player.getWorld() == null)
+		if (player.getWorld() == null)
 			console.fine("[isInPvPZone] player world is null!");
-		if(player.getWorld().getRaw() == null)
+		if (player.getWorld().getRaw() == null)
 			console.fine("[isInPvPZone] player raw world is null!");
-		if(player.getLocation() == null)
+		if (player.getLocation() == null)
 			console.fine("[isInPvPZone] player location is null!");
-		if(player.getLocation().getRaw() == null)
+		if (player.getLocation().getRaw() == null)
 			console.fine("[isInPvPZone] player raw location is null!");
 		RegionManager regionManager = worldGuard.getRegionManager(player.getWorld().getRaw());
-		if(regionManager == null)
+		if (regionManager == null)
 			console.fine("[isInPvPZone] regionManager is null!");
 		ApplicableRegionSet set = regionManager.getApplicableRegions(player.getLocation().getRaw());
 
-		if(set.size() == 0)
+		if (set.size() == 0)
 			return false;
 
 		return set.allows(DefaultFlag.PVP);
@@ -78,12 +77,12 @@ public class WorldGuardInterface
 	{
 		RegionManager regionManager = worldGuard.getRegionManager(player.getWorld().getRaw());
 		ApplicableRegionSet set = regionManager.getApplicableRegions(player.getLocation().getRaw());
-		if(set.size() == 0)
+		if (set.size() == 0)
 			return null;
 		StringBuilder sb = new StringBuilder();
-		for(ProtectedRegion r : set)
+		for (ProtectedRegion r : set)
 		{
-			if(sb.length() > 0)
+			if (sb.length() > 0)
 				sb.append(";");
 			sb.append(r.getId());
 		}
@@ -95,11 +94,11 @@ public class WorldGuardInterface
 		RegionManager regionManager = worldGuard.getRegionManager(location.getWorld().getRaw());
 		ApplicableRegionSet set = regionManager.getApplicableRegions(location.getRaw());
 
-		if(set.size() == 0)
+		if (set.size() == 0)
 			return null;
 
 		ArrayList<String> regions = new ArrayList<String>();
-		for(ProtectedRegion region : set)
+		for (ProtectedRegion region : set)
 			regions.add(region.getId());
 
 		return regions;
@@ -109,10 +108,10 @@ public class WorldGuardInterface
 	{
 		RegionManager regionManager = worldGuard.getRegionManager(player.getWorld().getRaw());
 		ApplicableRegionSet set = regionManager.getApplicableRegions(player.getLocation().getRaw());
-		if(set.size() == 0)
+		if (set.size() == 0)
 			return null;
 		ArrayList<String> regions = new ArrayList<String>();
-		for(ProtectedRegion r : set)
+		for (ProtectedRegion r : set)
 		{
 			regions.add(r.getId());
 		}
@@ -124,18 +123,18 @@ public class WorldGuardInterface
 		HashMap<String, Set<String>> result = new HashMap<String, Set<String>>();
 		RegionManager regionManager = worldGuard.getRegionManager(world.getRaw());
 		Map<String, ProtectedRegion> regions = regionManager.getRegions();
-		for(String region : regions.keySet())
+		for (String region : regions.keySet())
 			result.put(region, regions.get(region).getOwners().getPlayers());
 		return result;
 	}
 
 	public RunsafeLocation getRegionLocation(RunsafeWorld world, String name)
 	{
-		if(!serverHasWorldGuard())
+		if (!serverHasWorldGuard())
 			return null;
 
 		ProtectedRegion region = worldGuard.getRegionManager(world.getRaw()).getRegion(name);
-		if(region == null)
+		if (region == null)
 			return null;
 		BlockVector point = region.getMaximumPoint();
 		return new RunsafeLocation(world, point.getX(), point.getY(), point.getZ());
@@ -143,7 +142,7 @@ public class WorldGuardInterface
 
 	public Set<String> getOwners(RunsafeWorld world, String name)
 	{
-		if(!serverHasWorldGuard())
+		if (!serverHasWorldGuard())
 			return null;
 
 		return worldGuard.getRegionManager(world.getRaw()).getRegion(name).getOwners().getPlayers();
@@ -151,7 +150,7 @@ public class WorldGuardInterface
 
 	public Set<String> getMembers(RunsafeWorld world, String name)
 	{
-		if(!serverHasWorldGuard())
+		if (!serverHasWorldGuard())
 			return null;
 
 		return worldGuard.getRegionManager(world.getRaw()).getRegion(name).getMembers().getPlayers();
@@ -162,10 +161,52 @@ public class WorldGuardInterface
 		RegionManager regionManager = worldGuard.getRegionManager(world.getRaw());
 		ArrayList<String> regions = new ArrayList<String>();
 		Map<String, ProtectedRegion> regionSet = regionManager.getRegions();
-		for(String region : regionSet.keySet())
-			if(regionSet.get(region).getOwners().contains(player.getName()))
+		for (String region : regionSet.keySet())
+			if (regionSet.get(region).getOwners().contains(player.getName()))
 				regions.add(region);
 		return regions;
+	}
+
+	public List<String> getRegionsInWorld(RunsafeWorld world)
+	{
+		RegionManager regionManager = worldGuard.getRegionManager(world.getRaw());
+		return new ArrayList<String>(regionManager.getRegions().keySet());
+	}
+
+	public Map<String, Rectangle2D> getRegionRectanglesInWorld(RunsafeWorld world)
+	{
+		RegionManager regionManager = worldGuard.getRegionManager(world.getRaw());
+		Map<String, ProtectedRegion> regionSet = regionManager.getRegions();
+		HashMap<String, Rectangle2D> result = new HashMap<String, Rectangle2D>();
+		for (String regionName : regionSet.keySet())
+		{
+			Rectangle2D.Double area = new Rectangle2D.Double();
+			ProtectedRegion region = regionSet.get(regionName);
+			BlockVector min = region.getMinimumPoint();
+			BlockVector max = region.getMaximumPoint();
+			area.setRect(min.getX(), min.getZ(), max.getX() - min.getX(), max.getZ() - min.getZ());
+			result.put(regionName, area);
+		}
+		return result;
+	}
+
+	public boolean deleteRegion(RunsafeWorld world, String name)
+	{
+		RegionManager regionManager = worldGuard.getRegionManager(world.getRaw());
+		if(regionManager.getRegion(name) == null)
+			return false;
+		regionManager.removeRegion(name);
+		return true;
+	}
+
+	private WorldGuardPlugin getWorldGuard(Server server)
+	{
+		Plugin plugin = server.getPluginManager().getPlugin("WorldGuard");
+
+		if (plugin == null || !(plugin instanceof WorldGuardPlugin))
+			return null;
+
+		return (WorldGuardPlugin) plugin;
 	}
 
 	private IOutput console;
