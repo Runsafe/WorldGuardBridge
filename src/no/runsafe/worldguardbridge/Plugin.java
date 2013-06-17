76a65156bad0e2377bf3d95bb01229944d0ccda1
Kruithne <kruithne@gmail.com>
2013-06-17 01:23:49 +0100

    Changed to configurable plug-in to trigger config change events?

--- src/no/runsafe/worldguardbridge/Plugin.java
+++ src/no/runsafe/worldguardbridge/Plugin.java
@@ -1,9 +1,9 @@
 package no.runsafe.worldguardbridge;
 
-import no.runsafe.framework.RunsafePlugin;
+import no.runsafe.framework.RunsafeConfigurablePlugin;
 import no.runsafe.worldguardbridge.event.RegionBorderPatrol;
 
-public class Plugin extends RunsafePlugin
+public class Plugin extends RunsafeConfigurablePlugin
 {
 	@Override
 	protected void PluginSetup()
