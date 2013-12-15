595e62ad8ec5ee1608f95fc3e8727095a4b15aa5
Morten Nilsen <morten@runsafe.no>
2013-12-15 13:16:06 +0100

    Declare features used

--- src/no/runsafe/worldguardbridge/Plugin.java
+++ src/no/runsafe/worldguardbridge/Plugin.java
@@ -1,6 +1,8 @@
 package no.runsafe.worldguardbridge;
 
 import no.runsafe.framework.RunsafeConfigurablePlugin;
+import no.runsafe.framework.features.Events;
+import no.runsafe.framework.features.FrameworkHooks;
 import no.runsafe.worldguardbridge.event.RegionBorderPatrol;
 
 public class Plugin extends RunsafeConfigurablePlugin
@@ -8,6 +10,9 @@ public class Plugin extends RunsafeConfigurablePlugin
 	@Override
 	protected void PluginSetup()
 	{
+		addComponent(Events.class);
+		addComponent(FrameworkHooks.class);
+
 		exportAPI(getInstance(WorldGuardInterface.class));
 		this.addComponent(WorldGuardHooks.class);
 		this.addComponent(RegionBorderPatrol.class);
